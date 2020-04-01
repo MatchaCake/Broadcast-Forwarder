de5449c36a7398d32aefd5b36a0735fcb164fdb7
MatchaCake <li.6778@osu.edu>
2020-04-01 23:44:17 +0800

    Added function to handle hub challenge.

---
+++ callback/callback.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/feed", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		challenges, ok := r.URL.Query()["hub.challenge"]
+
+		if !ok || len(challenges[0]) < 1 {
+			log.Println("Url Param 'challenge' is missing")
+			return
+		}
+
+		// Query()["key"] will return an array of items,
+		// we only want the single item.
+		challenge := challenges[0]
+
+		log.Println("Url Param 'challenge' is: " + string(challenge))
+		_, err := w.Write([]byte(challenge))
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else if r.Method == http.MethodPost {
+
+	}
+}
