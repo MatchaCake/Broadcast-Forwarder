69c8cb16fb3ff3375ddda737ed183635f090c7de
MatchaCake <li.6778@osu.edu>
2020-04-01 21:11:39 +0800

    Added time parser.

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,20 @@
 package utils
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
-func parseTime(time str) string {
+func ParseTimeWithLocation(t string, loc *time.Location) string {
+	result, err := time.ParseInLocation(time.RFC3339, t, loc)
+	if err != nil {
+		log.Fatalf("error in parsing time to location:%v time:%v", loc.String(), err)
+	}
+	return result.String()
+}
 
-	time.Parse()
+func ParseToBeijingTime(t string) string {
+	secondsEastOfUTC := int((8 * time.Hour).Seconds())
+	beijing := time.FixedZone("Beijing Time", secondsEastOfUTC)
+	return ParseTimeWithLocation(t, beijing)
 }
